23771a1b9720249451ee4027ce52db7707f2c66e
Diego Flores <dfloresc1602@alumno.ipn.mx>
2024-12-12 20:40:04 -0600

    Agregando dominio del frontend en los cors

--- src/main/java/com/pt/gatewayms/config/CorsConfig.java
+++ src/main/java/com/pt/gatewayms/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
         config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("http://192.168.3.3:9090");
         config.addAllowedOrigin("https://black-wave-054ac8f10.5.azurestaticapps.net");
+        config.addAllowedOrigin("https://kind-pebble-0289a9d0f.4.azurestaticapps.net");
         config.addAllowedMethod("*"); // Permitir todos los métodos
         config.addAllowedHeader("*"); // Permitir todos los headers
         config.setAllowCredentials(true); // Permitir credenciales
